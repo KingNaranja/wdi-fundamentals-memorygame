bf0949867e35ec3d1bd90059b2a7424e7c110d98
KingNaranja <kledaa3@gmail.com>
2018-10-10 11:24:26 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,18 +5,37 @@ cards = ["queen", "queen","king","king"];
 
 cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!")
-    } else {
-        alert("Sorry, try again")
+        console.log("You found a match!");
+        } else {
+        console.log("Sorry, try again.");
+        }
+    };    
+           
+
+
+var flipCard = function(cardId) {
+    //this block will run when the user flips a card 
+
+    console.log("User flipped " + cards[cardId]);// logs flipped card to the console
+
+    cardsInPlay.push(cards[cardId]);
+    /*adds the selected card the the 'cards' array;
+    the parameter is the index number of the cards in the cards array.*/
+     
+    
+    if (cardsInPlay.length === 2) {
+        //if the two user-selected cards match, an alert will pop up
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert("You found a match!")
+        } else {
+            alert("Sorry, try again")
+        };
+        checkForMatch();
     };
-};
+    
+}
+
+flipCard(0);
+flipCard(2);
